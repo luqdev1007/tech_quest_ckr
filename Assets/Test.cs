d60b2b17f7a97cf608315f16b534e0891fc22ca2
LuQmu5 <luqdev1007@gmail.com>
2026-07-02 12:23:21 +0400

    import unitask package

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using Cysharp.Threading.Tasks;
+
 
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        UniTask test;
     }
 
     // Update is called once per frame
